5b74e9b035cc82828df5179a53c0d04a68fd68c7
kerry <116733173+kerryyys@users.noreply.github.com>
2023-05-12 11:37:46 +0800

    Create Main.java

---
+++ LAB TEST/labtest6 2023/Main.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import Friday.Data.*;
+import Friday.Feature.*;
+import Friday.Utils.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // * Initialize the features
+        Transaction transaction = new Transaction(Dataset.getTransactionTable());
+        Login login = new Login(Dataset.getLoginTable());
+        Fraudulent<Boolean> fraudulent = new Fraudulent<>(Dataset.getFraudTable());
+        Fraudulent<Double> scaleFraudulent = new Fraudulent<>(Dataset.getFraudScaleTable());
+        Fraudulent<Boolean> fraudulentCopy = new Fraudulent<>(Dataset.getFraudTable());
+        Fraudulent<Double> scaleFraudulentCopy = new Fraudulent<>(Dataset.getFraudScaleTable());
+
+        // * Perform data cleaning and transformation
+        transaction.dataCleaning().transformation();
+        login.dataCleaning();
+
+        fraudulent.dataCleaning().transformation(isFraud -> isFraud)
+                .merge(scaleFraudulent.dataCleaning().transformation(fraudScale -> fraudScale < 0.5 ? 0.0 : 1.0),
+                        fraudScale -> fraudScale >= 0.5);
+        scaleFraudulentCopy.dataCleaning()
+                .merge(fraudulentCopy.dataCleaning(), isFraud -> isFraud ? 1.0 : 0.0)
+                .transformation(fraudScale -> fraudScale < 0.5 ? 0.0 : 1.0);
+        List<List<?>> list = pointInTimeJoin(List.of(transaction, login), fraudulent);
+
+        // * Print the features
+        TableUtil.printFeatureTable(transaction, TableConstants.TRANSACTION_TITLE, TableConstants.TRANSACTION_LABELS);
+        TableUtil.printFeatureTable(login, TableConstants.LOGIN_TITLE, TableConstants.LOGIN_LABELS);
+        TableUtil.printFeatureTable(fraudulent, TableConstants.FRAUD_TITLE, TableConstants.FRAUD_LABELS);
+        TableUtil.printFeatureTable(scaleFraudulentCopy, TableConstants.FRAUD_SCALE_TITLE,
+                TableConstants.FRAUD_SCALE_LABELS);
+        TableUtil.printMergedTable(list, TableConstants.MERGED_TITLE, new String[] { "Deviation", "Coordinate", "isFraud" });
+    }
+
+    /**
+     * Performs point in time join for all the features input based on the input
+     * label
+     * and returns a list of list of values of all the features and the label. For
+     * every label value, point in time join will search for features' value of the
+     * latest date that is before the label date.
+     *
+     * @param features list of feature objects to perform point in time join on
+     * @param label    label feature object to use as reference for point in time
+     *                 join
+     * @return list of list of values of all the features and the label
+     */
+    public static List<List<?>> pointInTimeJoin(List<Feature<?>> features,
+            Feature<? extends Comparable<?>> label) {
+
+        for (Feature<?> feature : features)
+            feature.sortByDate();
+
+        List<List<?>> res = new ArrayList<>();
+        for (Data<?> labelData : label.dataList()) {
+            List<Object> row = new ArrayList<>();
+            String user = labelData.getUser();
+            int date = labelData.getDate();
+
+            for (Feature<?> feature : features) {
+                List<? extends Data<?>> featureData = feature.dataList();
+                if (featureData.isEmpty()) {
+                    res.add(null);
+                    continue;
+                }
+                Data<?> latest = featureData.get(0);
+                for (Data<?> value : featureData)
+                    if (value.getUser().equals(user) && value.getDate() <= date)
+                        latest = value;
+                row.add(latest.getData());
+            }
+            row.add(labelData.getData());
+            res.add(row);
+        }
+        return res;
+    }
+}
