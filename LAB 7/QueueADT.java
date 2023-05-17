fad936d9ba4d0750792b0841ab166daa1d4f6cbf
kerry <116733173+kerryyys@users.noreply.github.com>
2023-05-17 14:41:37 +0800

    Update QueueADT.java
    L7bQ1

--- LAB 7/QueueADT.java
+++ LAB 7/QueueADT.java
@@ -4,38 +4,39 @@ import java.util.Collections;
 
 public class QueueADT<E> {
     public static void main(String[] args) {
+
         int [] array={4,8,1,2,9,6,3,7};
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i : array) {
-            pq.add(i);
+            pq.offer(i);
         }
+        
         System.out.println("Elements in priority queue: "+pq.toString());
 
-        System.out.println("First element is removed from the priority queue");
-        pq.poll();
-        System.out.println(pq.toString());
+        System.out.println("\nFirst element which is removed from the priority queue: " + pq.poll());
+        System.out.println("\n" + pq.toString());
 
-        System.out.println("Adding 5 to the priority queue...");
-        pq.add(5);
+        System.out.println("\nAdding 5 to the priority queue...");
+        pq.offer(5);
         System.out.println(pq.toString());
 
         // Convert the priority queue into an array and display
         Object[] arr = pq.toArray();
-        System.out.print("Priority queue as an array: ");
+        System.out.print("\nPriority queue as an array: ");
         for (Object obj : arr) {
             System.out.print(obj + " ");
         }
         System.out.println();
 
-        System.out.println("First element in priority queue: "+pq.peek());
+        System.out.println("\nFirst element in priority queue: "+pq.peek());
 
-        System.out.println("Does the queue contains 1? "+pq.contains(1));
+        System.out.println("\nDoes the queue contains 1? "+pq.contains(1));
 
-        System.out.println("Size of the priority queue: "+ pq.size());
+        System.out.println("\nSize of the priority queue: "+ pq.size());
 
         // Remove elements from the priority queue until it is empty
-        System.out.print("Removing elements from priority queue: ");
+        System.out.print("\nRemoving elements from priority queue: ");
         while (!pq.isEmpty()) {
             System.out.print(pq.poll() + " ");
         }
@@ -44,8 +45,12 @@ public class QueueADT<E> {
         // Reversing the priority queue
         PriorityQueue<Integer> reversedPq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < array.length; i++) {
-            reversedPq.add(array[i]);
+            reversedPq.offer(array[i]);
         }
-        System.out.println("Reversed priority queue: " + reversedPq.toString());
+        while (!reversedPq.isEmpty()) { //to print in order
+            System.out.print(reversedPq.poll() + " ");
+        }
+
+        // System.out.println("Reversed priority queue: " + reversedPq.toString());
     }
 }
